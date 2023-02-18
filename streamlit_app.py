e8234646ea797ba266042afb251053013cbb664c
P4welSzymanski <125744124+P4welSzymanski@users.noreply.github.com>
2023-02-18 14:57:14 +0100

    Update streamlit_app.py
    editing menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,6 @@ import streamlit
 streamlit.title('My Parents New Healthy Dinner')
 streamlit.header('Breakfast MENU')
 
-
+streamlit.text('something1')
+streamlit.text('next thing')
+streamlit.text('another thing')
