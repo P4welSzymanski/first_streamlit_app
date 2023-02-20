bbf5f0c53fce201e85ead4c4c13e22c8d94c3874
P4welSzymanski <125744124+P4welSzymanski@users.noreply.github.com>
2023-02-20 12:33:00 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
-streamlit.header('\N{flexed biceps}🍌🥭 Build Your Own Fruit Smoothie 🥝🍇\N{flexed biceps}')
+streamlit.header('\N{banana}🍌🥭 Build Your Own Fruit Smoothie 🥝🍇\N{flexed biceps}')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
