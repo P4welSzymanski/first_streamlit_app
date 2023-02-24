0d388ff480b0393d7f4bd0b3984a41eb335700f9
P4welSzymanski <125744124+P4welSzymanski@users.noreply.github.com>
2023-02-24 13:44:57 +0100

    Update streamlit_app.py
    adding 'https://fruityvice.com/api/fruit/watermelon' response

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-import streamlit, pandas
+import streamlit, pandas, requests
 
 streamlit.title('My Parents New Healthy Dinner')
 streamlit.header('Breakfast Favorites')
@@ -21,3 +21,5 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
 
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
